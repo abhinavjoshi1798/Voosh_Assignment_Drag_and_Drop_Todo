e6525e13fcdd605107f8793be46509290a1e2636
Abhinav Joshi <65169622+abhinavjoshi1798@users.noreply.github.com>
2024-11-04 08:31:38 +0530

    not found page updated

--- fe/src/Pages/NotFound.jsx
+++ fe/src/Pages/NotFound.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
+import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div>
-      Not Found
+    <div className='min-vh-100 d-flex justify-content-center align-items-center flex-column'>
+      <img src="/images/404.svg" className='img-fluid' style={{width:"500px",height:"500px"}}
+      alt="404 image" />
+      <Link to="/login"><Button className='btn-primary'>Login</Button></Link> 
     </div>
   )
 }
